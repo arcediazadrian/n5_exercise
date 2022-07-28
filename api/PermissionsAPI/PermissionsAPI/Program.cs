d641c411b6d4246c50a95820f0ff6fac443e6b5d
arcediazadrian <arce.diaz.adrian@gmail.com>
2022-07-27 23:50:07 -0500

    initial axios request

--- api/PermissionsAPI/PermissionsAPI/Program.cs
+++ api/PermissionsAPI/PermissionsAPI/Program.cs
@@ -7,6 +7,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAllOrigins");
 
 app.UseAuthorization();
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,45 @@
 import logo from './logo.svg';
 import { Typography } from '@mui/material';
+import axios from "axios";
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [permissions, setPermissions] = useState([]);
+  const [permissionTypes, setPermissionTypes] = useState([]);
+
+
+  const getInitialData = async () => {
+    const permissionsResult = await axios.get('https://localhost:7204/api/Permissions')
+    const permissionTypesResult = await axios.get('https://localhost:7204/api/PermissionTypes')
+
+    console.log(permissionsResult, permissionTypesResult);
+
+    setPermissions(permissionsResult.data);
+    setPermissionTypes(permissionTypesResult.data);
+  }
+
+  useEffect(() => {
+    getInitialData();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <Typography variant="h1" component="h2">
-          h1. Heading
-        </Typography>
+        {permissions.map(permission => (
+          <div key={permission.grantedDate}>
+            <Typography variant="h1" component="h2">
+              {`${permission.employeeFirstName} ${permission.employeeLastName}`}
+            </Typography>
+          </div>
+        ))}
+        {permissionTypes.map(permissionType => (
+          <div key={permissionType.id}>
+            <Typography variant="h1" component="h2">
+              {permissionType.description}
+            </Typography>
+          </div>
+        ))}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
